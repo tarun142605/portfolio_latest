d247b84374f585b5286edea31be9b8be954fade3
Prayas Jadaun <prayashthakur6@gmail.com>
2024-11-04 20:58:26 +0530

    Change Picture and Intro

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,7 +13,8 @@ const About = () => {
             <div className="about__data">
                 <Info />
                 <p className="about__description"> 
-                Hey, my name is Prayas Jadaun. I adore building dynamic, user-focused applications, and I'm an eager Flutter developer. Designing scalable and effective solutions is my goal, and I have a strong foundation in AWS Cloud Computing. Because of my commitment to lifelong learning, I'm constantly experimenting with new technologies and pushing the envelope in order to develop meaningful, user-driven experiences. Together, let's create something truly remarkable.
+                Part-time widget whiz, full-time Flutter fan! Equipped with my reliable set of tools, which includes widgets and Bloc patterns, I approach the field of app creation with enthusiasm and accuracy. A well-designed software, in my opinion, is like the ideal animation: responsive, fluid, and astounds users. When I'm not creating dynamic user interfaces, you can probably find me battling layout restrictions or state management issues. Every application I create using Flutter is an experience in creating something aesthetically pleasing, pixel-perfect, and intuitive!
+            
                 </p>
 
                 <a download="" href={CV} className="button button--flex"> Download Resume
--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -58,7 +58,7 @@ const Data = () => {
     <h3 className="home__subtitle">
         Flutter Developer
     </h3>
-    <p className="home__description">Part-time widget whiz, full-time Flutter fan! Equipped with my reliable set of tools, which includes widgets and Bloc patterns, I approach the field of app creation with enthusiasm and accuracy. A well-designed software, in my opinion, is like the ideal animation: responsive, fluid, and astounds users. When I'm not creating dynamic user interfaces, you can probably find me battling layout restrictions or state management issues. Every application I create using Flutter is an experience in creating something aesthetically pleasing, pixel-perfect, and intuitive!</p>
+    <p className="home__description">Hey, my name is Prayas Jadaun. I adore building dynamic, user-focused applications, and I'm an eager Flutter developer. Designing scalable and effective solutions is my goal, and I have a strong foundation in AWS Cloud Computing. Because of my commitment to lifelong learning, I'm constantly experimenting with new technologies and pushing the envelope in order to develop meaningful, user-driven experiences. Together, let's create something truly remarkable.</p>
     <a href="#contact" className="button button--flex">
         Say Hello
         <svg
