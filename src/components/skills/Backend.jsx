979276c9077056283372f659045239cd58735f1f
Prayas Jadaun <prayashthakur6@gmail.com>
2024-11-06 19:21:12 +0530

    Update Skills

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -4,17 +4,17 @@ const Backend = () => {
   return (
     <div className="skills__content">
     <h3 className="skills__title">
-        Backend Developer
+        {/* Backend Developer */}
     </h3>
     <div className="skills__box">
         <div className="skills__group">
-            <div className="skills__data">
+            {/* <div className="skills__data">
                 <i className='bx bx-badge-check'></i>
                 <div>
                     <h3 className="skills__name">Dart</h3>
                     <span className="skills__level">Intermediate</span>
                 </div> 
-            </div>
+            </div> */}
             <div className="skills__data">
                 <i className='bx bx-badge-check'></i>
                 <div>
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -4,7 +4,7 @@ const Frontend = () => {
   return (
     <div className="skills__content">
     <h3 className="skills__title">
-        Frontend Developer
+        {/* Frontend Developer */}
     </h3>
     <div className="skills__box">
         <div className="skills__group">
