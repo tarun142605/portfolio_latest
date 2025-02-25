ba44f67022bcfae93fdebb754605bc7d4dcd979d
tarun142605 <tarun142605@gmail.com>
2025-02-25 14:11:18 +0530

    project

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -7,14 +7,14 @@ const Info = () => {
         <i className='bx bx-award about__icon'></i>
         <h3 className='about__title'>Experience</h3>
         <span className="about__subtitle">
-            1 year working with Flutter.
+            1 year working with Web Development.
         </span>
         </div>
         <div className="about__box">
         <i className='bx bx-briefcase-alt about__icon'></i>
         <h3 className='about__title'> Projects</h3>
         <span className="about__subtitle">
-            Android App Projects
+            Website Projects
         </span>
         </div>
         <div className="about__box">
